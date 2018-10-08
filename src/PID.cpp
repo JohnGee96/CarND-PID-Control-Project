07ccc2a8baf701c9ee9681f279b9dbf222a381f4
JohnGee96 <liujianji3@gmail.com>
2018-10-07 23:53:55 -0400

    Initial Implementation of the PID

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,21 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    this->Kp = Kp;
+    this->Ki = Ki;
+    this->Kd = Kd;
+    p_error = i_error = d_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
+    double prev_error = p_error;
+    double dt = 1.0; // assume dt is 1 unit of interval?
+    p_error = cte;
+    i_error += cte * dt;
+    d_error = (cte - prev_error) / dt;
 }
 
 double PID::TotalError() {
+    return -(Kp * p_error) - (Ki * i_error) - (Kd * d_error);
 }
 
